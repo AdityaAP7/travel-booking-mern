ad2c49bdde38c6a97ebdab94dd9adcb2b6351c8c
Aditya Palodkar <96766033+AdityaAP7@users.noreply.github.com>
2025-01-01 23:16:24 +0530

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -18,6 +18,10 @@
             user: null,
             loading: true,
             error: null
+         }  return {
+            user: null,
+            loading: true,
+            error: null
          }import { createContext, useEffect, useReducer } from 'react'
 
 const initial_state = {
